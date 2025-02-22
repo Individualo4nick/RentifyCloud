25270ab0672116cd4e472f84213b30d964dfaa0a
Vitaliy <modecom.pirogenko@gmail.com>
2025-02-22 15:43:07 +0300

    add eureka server

---
+++ build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+    id("org.springframework.boot") version("3.2.0")
+    id("io.spring.dependency-management") version("1.1.4")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_17
+}
+
+repositories {
+    mavenCentral()
+}
+
+val springCloudVersion = "2023.0.0"
+
+dependencies {
+    implementation("org.springframework.cloud:spring-cloud-starter-netflix-eureka-server")
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${springCloudVersion}")
+    }
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
