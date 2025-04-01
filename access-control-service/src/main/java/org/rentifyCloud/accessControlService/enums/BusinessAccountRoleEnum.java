a6b7a48818e87ac448241d86d52abf503ff29351
Vitaliy <modecom.pirogenko@gmail.com>
2025-04-02 00:15:18 +0300

    rename 2

--- access-control-service/src/main/java/org/rentifyCloud/accessControlService/enums/BusinessAccountRoleEnum.java
+++
@@ -1,6 +0,0 @@
-package org.rentifyCloud.accessControlService.enums;
-
-public enum BusinessAccountRoleEnum {
-    RENTIFY_ROLE_BA__ADMIN,
-    RENTIFY_ROLE_BA__STOREKEEPER
-}
---
+++ access-control-service/src/main/java/org/rentifyCloud/accessControlService/enums/CompanyAccountRoleEnum.java
@@ -0,0 +1,6 @@
+package org.rentifyCloud.accessControlService.enums;
+
+public enum CompanyAccountRoleEnum {
+    RENTIFY_ROLE_CA__ADMIN,
+    RENTIFY_ROLE_CA__STOREKEEPER
+}
